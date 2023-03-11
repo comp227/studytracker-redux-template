b35d9c6ce476ec0c305e336411ecce5eb5a1b79b
Osvaldo Jimenez <onzfonz@users.noreply.github.com>
2023-03-11 14:40:31 -0800

    Update Notification.js

--- quipquery/src/components/Notification.js
+++ quipquery/src/components/Notification.js
@@ -1,9 +1,8 @@
 const Notification = () => {
   const style = {
-    border: 'solid',
     padding: 10,
-    borderWidth: 1,
-    marginBottom: 5
+    backgroundColor: "lavender",
+    marginBottom: 10
   }
   
   if (true) return null
